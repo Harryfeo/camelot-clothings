90273093370bb1a10d7af6defd07dcdbf87b133d
Tobi <oluwatobiadara@gmail.com>
2020-07-29 11:13:00 +0100

    updated our header with styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {Switch, Route} from 'react-router-dom';
 
 import ShopPage from './pages/shop/shop.component.jsx';
 import HomePage from './pages/homepage/Homepage.component';
+import Header from './components/header/header.component.jsx';
+
 
 import './App.css';
 
@@ -10,6 +12,7 @@ import './App.css';
 function App() {
   return (
    <div>
+     <Header/>
      <Switch>
         <Route exact path="/"  component={HomePage} />
         <Route path="/shop"  component={ShopPage} />
